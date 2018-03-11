498cf1b4ee39f9061c3366ca1309de680316504c
parvathy <pmoha4@unh.newhaven.edu>
2018-03-11 18:03:29 -0400

    Changes after meeting with Frank - help and save

--- Timeline3.py
+++ Timeline3.py
@@ -60,38 +60,63 @@ class Main(tk.Frame):
         self.inner_fields_frame_1.pack(side=tk.TOP, fill="x")
         self.__data_file_input = Input(self.inner_fields_frame_1, 'CSV File', action=self.select_file);
 
+    def show_loading(self):
+        self.inner_fields_frame_2 = tk.Frame(self.input_label_frame);
+        self.inner_fields_frame_2.pack(side=tk.BOTTOM, fill="x")
+        self.loading_label = tk.Label(self.master, text='Loading...Please Wait..')
+        self.loading_label.config(font=("Calibri", 16))
+        self.loading_label.pack(side=tk.TOP, anchor="n")
+
+    def hide_loading(self):
+        self.loading_label.destroy();
+
     def select_file(self):
         """Selects a file, get data, convert to dataframe and load it to table"""
         self.__file_name = tk.filedialog.askopenfilename(initialdir=os.getcwd(), \
                                                 filetypes=(('CSV files', 'csv'),), title="Select Input CSV File.")
+        self.show_loading();
         self.__data_file_input.set_data(self.__file_name)
         self.csv_data = None
-        self.csv_data = pd.read_csv(self.__file_name, low_memory = False)
+        self.master.update();
         try:
+            self.csv_data = pd.read_csv(self.__file_name, low_memory=False)
             self.csv_data['date'] = pd.to_datetime((self.csv_data['date'] + " " + \
                                                     self.csv_data['time']),infer_datetime_format=True, errors='coerce');
         except Exception as e:
+            self.hide_loading();
             print('Exception ', e)
             tk.messagebox.showerror(message="No column named date, please check your CSV file.");
         else:
+            self.hide_loading();
             self.csv_data = self.csv_data.drop(['time'], axis=1)
+            self.current_data = self.csv_data;
             self.load_btns()
+
+    def save_csv(self):
+        filename = tk.filedialog.asksaveasfilename(initialdir=os.getcwd(), \
+                    initialfile='Timeline.csv', filetypes=(('CSV files', 'csv'),), \
+                                            title="Select Input CSV File.", defaultextension='*.csv')
+        print(filename);
+        try:
+            self.current_data.to_csv(filename, encoding='utf-8');
+        except Exception as e:
+            tk.messagebox.showerror(message="Sorry, Could not save!");
+            print(e);
+        else:
+            tk.messagebox.showinfo("Success", "CSV file saved to "+filename);
 
     def reset(self):
         """Reset the data to the initial data loaded from the CSV"""
         self.table_frame.destroy();
         self.inner_fields_frame_2.destroy();
+        self.show_loading();
         self.master.update();
-        self.csv_data = pd.read_csv(self.__file_name, low_memory = False)
-        self.csv_data['date'] = pd.to_datetime((self.csv_data['date'] + " " + self.csv_data['time']),
-                                               infer_datetime_format=True, errors='coerce');
-        self.csv_data = self.csv_data.drop(['time'], axis=1)
+        self.current_data = self.csv_data;
+        self.hide_loading();
         self.load_btns()
 
     def load_btns(self):
         """load buttons like filter, search and query builder"""
-        self.inner_fields_frame_2 = tk.Frame(self.input_label_frame);
-        self.inner_fields_frame_2.pack(side=tk.BOTTOM, fill="x")
         filter_label = tk.Label(self.inner_fields_frame_2, text='Filter Columns')
         filter_label.config(font=("Calibri", 12))
         filter_label.pack(side="left");
@@ -99,27 +124,34 @@ class Main(tk.Frame):
         self.filter_value = tk.Entry(self.inner_fields_frame_2, textvariable=self.filter_value)
         self.filter_value.config(font=("Calibri", 14), width=int(config_dict['entry_width']))
         self.filter_value.pack(side="left");
-        self.load_table(self.csv_data);
+        self.load_table();
         filter = tk.Button(self.inner_fields_frame_2, text="Filter", \
                            width=int(config_dict['button_width']), command=(lambda :self.filter(self.filter_value.get())))
         filter.pack(side='left')
-        #query_builder = CustomButton(self.input_label_frame, 'Query Builder', 650, 50, 180, self.filter_query_window)
+        help = tk.Button(self.inner_fields_frame_2, width=20, height=20, command=self.help_window)
+        img = tk.PhotoImage(file="help.png", width=20, height=20)
+        help.config(image=img)
+        help.image = img;
+        help.pack(side='left')
         search = tk.Button(self.inner_fields_frame_2, text="Search", width = int(config_dict['button_width']), \
                            command=self.search_window)
         search.pack(side='left')
         reset = tk.Button(self.inner_fields_frame_2, text="Reset", width = int(config_dict['button_width']), \
                           command=self.reset)
         reset.pack(side='left')
+        save = tk.Button(self.inner_fields_frame_2, text="Save as CSV", width=int(config_dict['button_width']), \
+                          command=self.save_csv)
+        save.pack(side='left')
 
-    def load_table(self, data=None):
+    def load_table(self):
         """Loads the table - excel sheet like"""
-        if data is None or len(data) == 0:
+        if self.current_data is None or len(self.current_data) == 0:
             tk.messagebox.showerror(message="No data to show!");
             return;
         self.table_frame = tk.Frame(self.master)
         self.table_frame.config();
         self.table_frame.pack(anchor="c", fill=tk.BOTH, expand="YES")
-        self.table = MyTable(self.table_frame, data);
+        self.table = MyTable(self.table_frame, self.current_data);
         self.table.model.df = self.table.model.df.reset_index(drop=True)
         self.table.maxcellwidth = int(config_dict['max_cell_width'])
         self.table.show()
@@ -139,6 +171,18 @@ class Main(tk.Frame):
         self.table.setRowColors(win_prefetch_highlight, '#FF0000', 'all')
         self.table.setRowColors(sys_highlight, '#0070C0', 'all')
 
+    def help_window(self):
+        """Displays a search window box"""
+        lines = ['You may enter the query to filter the data based on column names.',\
+                 'The comparison operators you may use include ==, <, > and !=.',
+                 'The values should be in single quotes.',
+                 'Paranthesis, and, or opertors work.',
+                 '', 'A few of the examples are:',
+                 "date > '2017-01-01' and date < '2018-01-01'",
+                 "date == '2015-03-16 10:53:00'",
+                 "type=='ctime' or type == 'atime'"]
+        #self.master.option_add('*Dialog.msg.font', 'Calibri 14')
+        tk.messagebox.showinfo('Help on Filter', "\n".join(lines), )
 
     def search_window(self):
         """Displays a search window box"""
@@ -164,75 +208,17 @@ class Main(tk.Frame):
         self.search_frame.pack(side="top", fill="both", expand=True, padx=5, pady=5)
         self.search_entry.focus_set();
 
-    def filter_query_window(self):
-        """TODO: Query Builder"""
-        self.child_win = tk.Toplevel(self.master)
-        self.child_win.wm_title("Build Filter Query")
-        self.child_win.geometry("%dx%d%+d%+d" % (600, 400, 250, 125))
-        self.headers = list(self.csv_data.columns.values)
-
-        self.frame = tk.LabelFrame(self.child_win, text="Filter Query Builder")
-        self.frame.config(font=("Calibri", 14))
-        self.frame.propagate(0)
-        self.filter_cols = []
-        self.filter_values = []
-        for num in range(5):
-            self.child_frame = tk.Frame(self.frame);
-            self.query_filter = tk.StringVar(self.frame)
-            self.header_options = tk.OptionMenu(self.child_frame, self.query_filter, *self.headers)
-            self.header_options.config(width=15)
-            self.header_options.pack(side=tk.LEFT)
-            self.query_value = tk.StringVar()
-            self.query_value = tk.Entry(self.child_frame, textvariable=self.query_value)
-            self.query_value.config(width=80)
-            self.query_value.pack(side=tk.RIGHT)
-            equal_label = tk.Label(self.child_frame, text=' = ')
-            equal_label.config(font=("Calibri", 12))
-            equal_label.pack();
-            self.child_frame.pack(side="top", fill="both", padx=5, pady=5)
-            self.filter_cols.append(self.query_filter)
-            self.filter_values.append(self.query_value)
-        #btn = CustomButton1(self.frame, title="Buid Query", action=self.create_query)
-        self.frame.pack(side="top", fill="both", expand=True, padx=20, pady=20)
-
-    def create_query(self):
-        """TODO: Query Builder"""
-        query = '';
-        date_range1 = None
-        date_range2 = None
-        for i in range(5):
-            if not self.filter_cols[i].get() is '':
-                if i > 0:
-                    query += ' and '
-                field_name = self.filter_cols[i].get();
-                field_value = self.filter_values[i].get();
-                if field_name == 'date':
-                    if '-' in field_value:
-                        date_ranges = field_value.split('-')
-                        date_range1 = date_ranges[0]
-                        date_range2 = date_ranges[1]
-                        date_range1 = pd.to_datetime(date_range1, infer_datetime_format=True, errors='raise').date
-                        date_range2 = pd.to_datetime(date_range2, infer_datetime_format=True, errors='raise').date
-                        query += field_name + ' >= ' + '@date_range1' + ' and ' + field_name + ' <= @date_range2';
-                    else:
-                        date_range1 = pd.to_datetime(field_value, infer_datetime_format=True, errors='raise').date
-                        query += field_name + '==' + '@date_range1'
-                else:
-                    query += field_name + '==' + '"' + field_value + '"'
-        self.filter_value.insert(0, query)
-        self.child_win.destroy()
-        self.filter(query, date_range1, date_range2)
-        return;
-
     def search(self, srch_value):
         """Performs case insensitive search on data loaded from CSV"""
         self.table_frame.destroy();
+        self.show_loading();
         self.master.update();
         self.search_win.destroy();
         mask = np.column_stack(\
             [self.csv_data[col].astype(str).str.lower().astype(str).str.contains(srch_value.lower(), na=False) for col in self.csv_data])
-        self.csv_data_1 = self.csv_data.loc[mask.any(axis=1)]
-        self.load_table(data=self.csv_data_1);
+        self.current_data = self.csv_data.loc[mask.any(axis=1)]
+        self.hide_loading();
+        self.load_table();
 
     def filter(self, query):
         """Filter data based on the query"""
@@ -240,28 +226,18 @@ class Main(tk.Frame):
             tk.messagebox.showerror(message="Empty query!");
             return;
         self.table_frame.destroy()
+        self.show_loading();
+        self.master.update();
         print('Query : ', query);
         try:
-            if 'date' in query:
-                query_arr = shlex.split(query, posix=False)
-                indx = -1;
-                date_found = False;
-                for part in query_arr:
-                    indx += 1
-                    if (part == 'date') and (query_arr[indx+1] == '==' or query_arr[indx+1] == '!='):
-                        if(len(query_arr[indx+2].split()) == 1):
-                            query_arr[indx] = "date.dt.strftime('%Y-%m-%d')"
-                            date_found = True
-
-                if date_found:
-                    query = ' '.join(query_arr)
-            print(query)
-            self.csv_data_1 = self.csv_data.query(query)
+            self.current_data = self.csv_data.query(query)
         except Exception as e:
+            self.hide_loading();
             print('Exception ', e)
             tk.messagebox.showerror(message="Check your query!");
         else:
-            self.load_table(data=self.csv_data_1);
+            self.hide_loading();
+            self.load_table();
 
 
 
@@ -271,7 +247,8 @@ if __name__ == '__main__':
     try:
         config_file = open("configuration.txt", 'r')
     except:
-        tk.messagebox.showerror(message="Please put the configuration.txt file in the same folder as python file.");
+        tk.messagebox.showerror(message="Configuration Error: \
+        The configuration.txt file should be in the same folder as python file.");
     else:
         config_dict = {}
         for row in config_file:
